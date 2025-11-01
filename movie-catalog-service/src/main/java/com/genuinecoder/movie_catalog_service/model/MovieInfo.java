f8e4f39e93249609383f0332341c5c0095de76eb
Prawin J <prawinj@salttechno.com>
2025-11-01 13:54:17 +0530

    Initial commit - added all microservices

---
+++ movie-catalog-service/src/main/java/com/genuinecoder/movie_catalog_service/model/MovieInfo.java
@@ -0,0 +1,54 @@
+package com.genuinecoder.movie_catalog_service.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MovieInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String path;
+    private String description;
+    private String name;
+
+
+}
